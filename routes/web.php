b0b24c3d54079fdc349f44fb60a515e805548a82
Bynntangg <bintang@gmail.com>
2025-08-03 16:29:19 +0700

    yyy

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('index');
+    return view('welcome');
 });
 Route::view('/sitemap', 'sitemap');
 Route::view('/privacy-policy', 'privacy-policy');
